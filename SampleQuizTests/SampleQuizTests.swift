efecdf1396a178e93a196d90a1778d338d0d9705
{kiki321321} <{motibutter@gmail.com}>
2025-06-09 15:04:46 +0900

    Initial Commit

---
+++ SampleQuizTests/SampleQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleQuizTests.swift
+//  SampleQuizTests
+//
+//  Created by furuse on 2025/06/09.
+//
+
+import Testing
+@testable import SampleQuiz
+
+struct SampleQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
